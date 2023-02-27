a9613d6bf85d1aeebca3e81ce1a6e25800ec4c0b
Bruzzone Marcelo Daniel <bruzzone1804@gmail.com>
2023-02-27 17:12:15 -0300

    agregados js

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+
+let miNombre = 'marcelo';
+let miApellido = 'Bruzzone';
+let miEdad = '36';
+let miMascota = 'lara';
+let edadMascota = '2';
+
+console.log(miNombre)
+console.log(miApellido)
+console.log(miEdad)
+console.log(miMascota)
+console.log(edadMascota)
+
+let nombreCompleto = miNombre + ' ' + miApellido;
+
+let textoPresentacion = 'Hola soy '+ miNombre +' '+ miApellido +' '+ ', tengo'+ miEdad+' años.'+ ' mi mascota se llama '+miMascota;
+
+let sumaEdades = miEdad + edadMascota
+let restaEdades = miEdad - edadMascota
+let productoEdades = miEdad*edadMascota
+let divisionEdades = miEdad/edadMascota
+
+//let yo = prompt('Cual es tu nombre?');
+
+//let nombre = prompt('cual es su nombre?');
+//let apellido = prompt('cual es su apellido?');
+//let edad = prompt('cual es su edad?');
+//let mascota = prompt('cual es el nombre de su mascota?');
+//let edadmascota = prompt('cual es la edad de su mascota?');
+
+//alert(nombre)
+//alert(apellido)
+//alert(edad)
+//alert(mascota)
+//alert(edadmascota)
+
+let alumno = {}
+//alumno.nombre = prompt('Ingrese el nombre del alumno')
+//alumno.apellido = prompt('Ingrese el apellido del alumno')
+//alumno.edad= prompt('Ingrese la edad del alumno')
+//alumno.sexo= prompt('Ingrese el sexo del alumno')
+//alumno.curso= prompt('Ingrese el curso del alumno')
+
+//console.table(alumno)
